80f61da61e848dbc73297dc7dd8176fdfec9028f
Craig Gardner <craig.gardner@cdk.com>
2015-05-27 23:09:49 -0700

    Added Middleware to check project permissions.

---
+++ app/Http/Middleware/ProjectPermissions.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Http\Middleware;
+
+use App\Models\Project;
+use Closure;
+use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class ProjectPermissions implements Middleware
+{
+    private $project;
+
+    public function __construct(Project $project)
+    {
+        $this->project = $project;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $projectId = $request->route()
+            ->getParameter('projects', false);
+        if (!$projectId) {
+            return new RedirectResponse(route('projects.index'));
+        }
+
+        $project = $this->project
+            ->newQuery()
+            ->find($projectId);
+
+        $user = Auth::User();
+        if ($user->company_id != $project->company_id) {
+            return new RedirectResponse(route('projects.index'));
+        }
+
+        return $next($request);
+    }
+}
--- app/Http/Middleware/UserHasCompanyMiddleware.php
+++ app/Http/Middleware/UserHasCompanyMiddleware.php
@@ -1,24 +1,25 @@
 <?php namespace App\Http\Middleware;
 
-use Closure;
 use Auth;
+use Closure;
 use Redirect;
 
-class UserHasCompanyMiddleware {
-    
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-  public function handle($request, Closure $next)
-  {
-    if(Auth::User()->company_id == '' || Auth::User()->company_user_status != 'active') {
-      return Redirect::to('welcome/company');
+class UserHasCompanyMiddleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::User()->company_id == '' || Auth::User()->company_user_status != 'active') {
+            return Redirect::to('welcome/company');
+        }
+        return $next($request);
     }
-    return $next($request);
-  }
 
 }
