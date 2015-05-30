9c6db6ccf8c3b58d85015a898482af8d821eac1a
Anthony <anthony@aecore.com>
2015-05-30 08:37:39 -0700

    Preliminary project navigation.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,4 +1,16 @@
-@extends('layouts.application.main')
+@extends('layouts.application.main_wide')
+@extends('layouts.application.nav_project')
 @section('content')
-    This is the Project Dashboard for {!! $project->name !!}
+  <div class="page-wrapper">
+    <div class="pagehead">
+      <div class="container-fluid">
+        <span class="btn btn-primary pull-left toggle-nav" style="margin-right:10px;padding:7px;" onClick="$('#projectnav').toggle();"><span class="glyphicon glyphicon-menu-hamburger"></span></span>
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      This is the Project Dashboard for {!! $project->name !!}
+    </div>
+  </div>
 @endsection
