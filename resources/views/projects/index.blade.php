b05d839cc121ae595b5c247a0097d1d5182db180
Anthony <anthony@aecore.com>
2015-05-30 19:06:10 -0700

    Added tiled project list view.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,17 +1,9 @@
 @extends('layouts.application.main')
 @section('content')
-  
   <div class="pagehead">
     <a href="/projects/create" type="button" class="btn btn-sm btn-success pull-right"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> New Project</a>
     <h1>Projects</h1>
   </div>
-  
-	<div class="row">
-    <div class="col-sm-8 col-md-9">
-      @include('projects.partials.project_list')
-    </div>
-		<div class="col-sm-4 col-md-3 mobile-hide">
-      @include('projects.partials.project_filter')
-		</div>
-  </div>
+  @include('projects.partials.project_filter')
+  @include('projects.partials.project_list_tiled')
 @endsection
--- resources/views/projects/partials/project_filter.blade.php
+++ resources/views/projects/partials/project_filter.blade.php
@@ -1,93 +1,14 @@
-<script type="text/javascript">
-  $(document).ready(function(){
-    $("#project_size_text").html("0 - 50000"); //set default size
-    $("#project_size_slider").slider({
-      range:true,
-      min:0,
-      max:50000,
-      values:[0,50000],
-        slide: function( event, ui ) {
-          $("#project_size_text").html(ui.values[0] + " - " + ui.values[1]);
-        }
-    });
-    
-    $("#project_value_text").html("$10000 - $1000000"); //set default value
-    $("#project_value_slider").slider({
-      range:true,
-      min:0,
-      max:1000000,
-      values:[10000,1000000],
-        slide: function( event, ui ) {
-          $("#project_value_text").html("$" + ui.values[0] + " - $" + ui.values[1]);
-        }
-    });
-    
-  });
-</script>
-
-<div class="panel panel-default">
-  <div class="panel-heading">
-    <span class="btn-link pull-right" onClick="">Reset All</span>
-    Filters
-  </div>
-  <div class="panel-body" style="padding:10px 15px;">
+<div class="form-inline table-filters">
     <div class="form-group">
-      {!! Form::label('project_search', 'Search', array('class' => 'control-label small bold', 'style'=>'margin:0;')) !!}
-      {!! Form::text('project_search', null, array('class' => 'form-control', 'placeholder' => 'Project name...' )) !!}
+        {!! Form::text('search', null, array('class' => 'form-control', 'style'=>'min-width:250px;', 'placeholder' => 'Search...' )) !!}
     </div>
-    
-    <div class="form-group">
-      {!! Form::label('project_status', 'Project Status', array('class' => 'control-label small bold', 'style'=>'margin:0;')) !!}
-      {!! Form::select('start_type', array(
-          'Bid' => 'Bid',
-          'Pre-construction' => 'Pre-construction',
-          'Under Construction' => 'Under Construction',
-          'Close Out' => 'Close Out',
-          'Archived' => 'Archived'
-        ), null, array('class'=>'form-control'))
-      !!}
-    </div>
-    
-    <div class="form-group">
-      {!! Form::label('project_size_text', 'Project Size', array('class' => 'control-label small bold', 'style'=>'margin:0;')) !!}
-      <p class="text-danger" id="project_size_text" style="margin-bottom:5px;"></p>
-      <div class="slider" id="project_size_slider"></div>
-    </div>
-    
-    <div class="form-group no-margin">
-      {!! Form::label('project_value_text', 'Project Value', array('class' => 'control-label small bold', 'style'=>'margin:0;')) !!}
-      <p class="text-danger" id="project_value_text" style="margin-bottom:5px;"></p>
-      <div class="slider" id="project_value_slider"></div>
-    </div>
-    
-    <!--
-    <div class="form-group">
-      {!! Form::label('start_type', 'Date Range', array('class' => 'control-label small bold')) !!}
-      <div class="form-inline">
-        <div class="form-group">
-          {!! Form::select('start_type', array(
-              'start_before' => 'Start Before',
-              'start_after' => 'Start After'
-            ), null, array('class'=>'form-control input-sm', 'style'=>'width:50%;'))
-          !!}
-          {!! Form::text('start_date', null, array('class' => 'form-control input-sm', 'style'=>'width:48%;', 'placeholder' => 'Date' )) !!}
-        </div>
+    <div class="btn-group">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">Test <span class="caret"></span></button>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="#">My Projects: Open</a></li>
+            <li><a href="#">My Projects: Archived</a></li>
+            <li><a href="#">All Projects: Open</a></li>
+            <li><a href="#">All Projects: Archived</a></li>
+        </ul>
       </div>
-    </div>
-    
-    <div class="form-group">
-      <div class="form-inline">
-        <div class="form-group">
-          {!! Form::select('finish_type', array(
-              'finish_before' => 'Finish Before',
-              'finish_after' => 'Finish After'
-            ), null, array('class'=>'form-control input-sm', 'style'=>'width:50%;'))
-          !!}
-          {!! Form::text('finish_date', null, array('class' => 'form-control input-sm', 'style'=>'width:48%;', 'placeholder' => 'Date' )) !!}
-        </div>
-      </div>
-    </div>
-    -->
-    
-  </div>
 </div>
