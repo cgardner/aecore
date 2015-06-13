6334dea422686a37827bafb2df4822a15fb718f5
Craig Gardner <craig.gardner@cdk.com>
2015-06-12 23:34:07 -0700

    Cleaned up the Project Controller
    Added View Composer for the project navigation so we could clean up the controller

---
+++ resources/views/layouts/application/nav_project.blade.php
@@ -0,0 +1,30 @@
+<div class="sidebar-nav" id="projectnav">
+   <span class="btn btn-primary pull-right toggle-nav" style="margin:5px 10px 0 0;padding:6px;"
+         onClick="$('#projectnav').toggle();">
+       <span class="glyphicon glyphicon-menu-hamburger" style="margin:0;"></span>
+   </span>
+
+    <div class="btn-group" style="width: 100%">
+        <button class="btn btn-default btn-block dropdown-toggle" data-toggle="dropdown">
+            #{{ $activeProject->number . ' ' . $activeProject->name }}
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" role="menu">
+            @foreach($projects as $project)
+                <li>
+                    {!! link_to_route('projects.show', '#' . $project->number . ' ' . $project->name, ['project' => $project->id]) !!}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+    <ul class="nav sidebar-nav">
+        <li class="nav-header">Project Navigation</li>
+        @foreach($navigation as $link)
+            <li>
+                <a href="{!! $link['url'] !!}" class="{{ \Request::url() == $link['url'] ? 'active' : null }}">
+                    <span class="glyphicon {{ $link['icon'] }}"></span> {{ $link['title'] }}
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
