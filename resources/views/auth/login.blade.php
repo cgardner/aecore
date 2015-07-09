ba86677fec6ef3a4dc3a8e3243147795c57624b5
acirinelli <anthony@aecore.com>
2015-07-08 20:43:49 -0700

    Updated boring sign up and log in pages.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.storefront.main')
 @section('content')
-    <div class="col-md-4 col-md-offset-4" style="padding-top:18%;">
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+             $.backstretch("{!! asset('/css/img/bg/onboarding-bg.jpg') !!}");
+        });
+    </script>
+    
+    <div class="col-md-4 col-md-offset-4" style="padding-top:16%;">
+        
         @if (Session::has('accountDeleted'))
             <script type="text/javascript" charset="utf-8">
                 setTimeout(function () {
@@ -18,28 +26,39 @@
             <div class="alert alert-warning"><span class="glyphicon glyphicon-alert"></span> {!! Session::get('warningMessage') !!} </div>
         @endif
         
-        <h4 class="text-muted">Log in to Aecore</h4>
-        {!! Form::open(array('url' => 'auth/login', 'method' => 'post', 'style'=>'margin-bottom:20px')) !!}
+        <div class="panel panel-default">
+            <div class="panel-body" style="padding:20px;">
+                <h3 class="text-info" style="margin:0;">Log in to Aecore.</h3>
+                <h5 class="text-muted" style="margin:7px 0 15px 0;">We're happy to have you back!</h5>
+                {!! Form::open(array('url' => 'auth/login', 'method' => 'post')) !!}
 
-            <div class="form-group">
-                <span class="text-danger">{!! $errors->first('email') !!}</span>
-                <div class="input-group">
-                    <span class="input-group-addon" id="addon-password"><span class="glyphicon glyphicon-envelope"></span></span>
-                    {!!Form::text('email', null, array('class'=>'form-control', 'placeholder'=>'you@yourdomain.com', 'aria-describedby' => 'addon-email', 'autofocus' => 'true'))!!}
-                </div>
+                    <div class="form-group">
+                        <span class="text-danger">{!! $errors->first('email') !!}</span>
+                        <div class="input-group">
+                            <span class="input-group-addon" id="addon-email"><span class="glyphicon glyphicon-envelope"></span></span>
+                            {!!Form::text('email', null, array('class'=>'form-control input-lg', 'placeholder'=>'you@yourdomain.com', 'aria-describedby' => 'addon-email', 'autofocus' => 'true'))!!}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <span class="text-danger">{!! $errors->first('password') !!}</span>
+                        <div class="input-group">
+                            <span class="input-group-addon" id="addon-password"><span class="glyphicon glyphicon-lock"></span></span>
+                            {!!Form::password('password',array('class' => 'form-control input-lg', 'placeholder' => 'Password', 'aria-describedby' => 'addon-password'))!!}
+                        </div>
+                        <p class="text-muted no-margin small">Did you forget your password? {!! link_to('password', 'Reset Password') !!}</p>
+                    </div>
+                    
+                    <div class="form-group no-margin">
+                        {!! Form::submit('Log In', array('class' => 'btn btn-block btn-lg btn-info')) !!}
+                    </div>
+                    
+                {!! Form::close() !!}
             </div>
-
-            <div class="form-group">
-                <span class="text-danger">{!! $errors->first('password') !!}</span>
-                <div class="input-group">
-                    <span class="input-group-addon" id="addon-password"><span class="glyphicon glyphicon-lock"></span></span>
-                    {!!Form::password('password',array('class' => 'form-control', 'placeholder' => 'Password', 'aria-describedby' => 'addon-password'))!!}
-                </div>
+            
+            <div class="panel-footer center">
+                <span class="text-muted">First time here? {!! link_to('signup', 'Create an Account', array('class'=>'text-muted bold')) !!}</span>
             </div>
-
-            {!! Form::submit('Log In', array('class' => 'btn btn-success')) !!}
-            {!! link_to('password', 'Forgot Password?', array('class'=>'text-muted btn-spacer-left')) !!}
-        {!! Form::close() !!}
-        <span>First time here? {!! link_to('signup', 'Create an Account', array('class'=>'bold')) !!}</span>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/storefront/main.blade.php
+++ resources/views/layouts/storefront/main.blade.php
@@ -11,6 +11,7 @@
     <script type="text/javascript" src="{!! asset('/js/jquery-2.1.3.min.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('/js/jquery-ui/jquery-ui.min.js') !!}"></script>
     <script type="text/javascript" src="{!! asset('/js/bootstrap.min.js') !!}"></script>
+    <script type="text/javascript" src="{!! asset('/js/jquery.backstretch.js') !!}"></script>
     <script type="text/javascript">
       $(function(){
         $('body').on('hidden.bs.modal', '.modal', function () {
